ff353fa1e2b3915334a0a528f2b9e9da67313573
josehr06 <josemanuelhenao2602@gmail.com>
2025-03-12 16:20:58 -0500

    Se arreglan los conflictos del merge

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import PanelControl from './pages/PanelControl'
-<<<<<<< HEAD
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-=======
->>>>>>> 6e50b29055121fd7e9f3a036daa41bdbc8c26264
 import './index.css'
 import Login from './pages/auth/Login'
 
--- src/pages/PanelControl.jsx
+++ src/pages/PanelControl.jsx
@@ -1,16 +1,10 @@
 import Card from '../components/Card'
 import MenuLateral from '../components/MenuLateral'
-<<<<<<< HEAD
 import { envios } from '../services/database'
 
 
-const PanelControl = () => {
   
-=======
-import { envios } from '../services/dataBase'
 const PanelControl = () => {
-
->>>>>>> 6e50b29055121fd7e9f3a036daa41bdbc8c26264
   return (
     <section className="panel-control">
       <MenuLateral />
@@ -26,5 +20,5 @@ const PanelControl = () => {
 }
 
 
-export default PanelControl
+export default PanelControl;
 
